a80662ea5c789e98a716eb01afdf6ef65375f393
lscheinhorn <lukescheinhorn@gmail.com>
2025-02-14 16:18:00 -0500

    fix: API-URL update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5001/tasks";
+const API_URL = "https://my-todo-app-mujx.onrender.com";
 
 function App() {
   const [tasks, setTasks] = useState([]);
