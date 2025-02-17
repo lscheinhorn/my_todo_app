d40d5af0e7f2ac49434a6da195aa26507d195d4f
lscheinhorn <lukescheinhorn@gmail.com>
2025-02-16 21:43:55 -0500

    fix: syntax error

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-const mongoURI = process.env.MONGO_URI || ;
+const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB connection error:", err));
